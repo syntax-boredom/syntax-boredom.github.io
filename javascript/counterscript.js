674b41fccb0092d7e1ea6b6421cd71701e2bc25f
Illusionvilage <128383087+Illusionvilage@users.noreply.github.com>
2023-11-29 20:42:30 +0100

    Create counterscript.js

---
+++ javascript/counterscript.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const counterButton = document.getElementById("counterButton");
+
+    // Check if the count is stored in local storage
+    let count = localStorage.getItem("counter") || 0;
+
+    // Display the initial count
+    updateCounterText();
+
+    // Function to update the counter text
+    function updateCounterText() {
+        counterButton.innerText = `This Button has been clicked ${count} times.`;
+    }
+
+    // Event listener for button click
+    counterButton.addEventListener("click", function () {
+        // Increase the count
+        count++;
+        console.log(count);
+
+        // Update the counter text
+        updateCounterText();
+
+        // Save the count to local storage
+        localStorage.setItem("counter", count);
+    });
+});
