b5ea6df68f8cd69af619e9355339fe326adc314f
Illusionvilage <128383087+Illusionvilage@users.noreply.github.com>
2023-11-29 21:41:33 +0100

    Update counterscript.js

--- javascript/counterscript.js
+++ javascript/counterscript.js
@@ -1,27 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     const counterButton = document.getElementById("counterButton");
-
-    // Check if the count is stored in local storage
-    let count = localStorage.getItem("counter") || 0;
-
-    // Display the initial count
-    updateCounterText();
 
     // Function to update the counter text
     function updateCounterText() {
         counterButton.innerText = `This Button has been clicked ${count} times.`;
     }
 
+    // Function to fetch the count from the JSON file
+    async function fetchCount() {
+        try {
+            const response = await fetch("path/to/counter.json");
+            const jsonData = await response.json();
+
+            // Extract the count from the JSON data
+            return jsonData.count || 0;
+        } catch (error) {
+            console.error("Error fetching count:", error);
+        }
+
+        return 0;
+    }
+
+    // Function to update the count in the JSON file
+    async function updateCount(newCount) {
+        try {
+            const response = await fetch("path/to/counter.json", {
+                method: "PUT", // Use the appropriate HTTP method for updating files
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ count: newCount }),
+            });
+
+            if (response.ok) {
+                count = newCount;
+                updateCounterText();
+            }
+        } catch (error) {
+            console.error("Error updating count:", error);
+        }
+    }
+
+    // Initialize count from the JSON file
+    count = await fetchCount();
+
+    // Display the initial count
+    updateCounterText();
+
     // Event listener for button click
-    counterButton.addEventListener("click", function () {
+    counterButton.addEventListener("click", async function () {
         // Increase the count
         count++;
-        console.log(count);
 
         // Update the counter text
         updateCounterText();
 
-        // Save the count to local storage
-        localStorage.setItem("counter", count);
+        // Update the count in the JSON file
+        await updateCount(count);
     });
 });
