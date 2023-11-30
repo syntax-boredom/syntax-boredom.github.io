40100bd0234a2b640b7dca142966eb1387d42c62
Illusionvilage <128383087+Illusionvilage@users.noreply.github.com>
2023-11-30 09:12:01 +0100

    Update counterscript.js

--- javascript/counterscript.js
+++ javascript/counterscript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const counterButton = document.getElementById("counterButton");
+    const resetButton = document.getElementById("resetCounter");
 
     // Check if the count is stored in local storage
     let count = localStorage.getItem("counter") || 0;
@@ -9,13 +10,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to update the counter text
     function updateCounterText() {
-        counterButton.innerText = `You have clicked the button ${count} times.`;
+        counterButton.innerText = `This Button has been clicked ${count} times.`;
     }
 
     // Event listener for button click
     counterButton.addEventListener("click", function () {
         // Increase the count
         count++;
+
+        // Update the counter text
+        updateCounterText();
+
+        // Save the count to local storage
+        localStorage.setItem("counter", count);
+    });
+
+    // Event listener for reset button click
+    resetButton.addEventListener("click", function () {
+        // Reset the count
+        count = 0;
 
         // Update the counter text
         updateCounterText();
